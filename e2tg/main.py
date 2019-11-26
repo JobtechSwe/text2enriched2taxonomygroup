1416bdd753e31df6bf7306b260dad3e27b9e39c1
Markus Sköld <markus.skold@arbetsformedlingen.se>
2019-11-26 14:47:47 +0100

    Adds parent navigation

--- e2tg/main.py
+++ e2tg/main.py
@@ -26,6 +26,44 @@ def load_groups_for_occupations(occupations, number_of_groups=3):
     return relations
 
 
+def load_sibling_groups(group_id):
+    # Get parent id from selected group
+    es_result = elastic.get(index=settings.ES_TAX_INDEX, id=group_id)
+    parent_label = es_result.get('_source').get('parent', {}).get('label')
+    parent_id = es_result.get('_source').get('parent', {}).get('concept_id')
+    dsl = {
+        "from": 0,
+        "size": 100,
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "term": {
+                            "parent.concept_id.keyword": parent_id
+                        }
+                    },
+                    {
+                        "term": {
+                            "type": "occupation-group"
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    es_result = elastic.search(index=settings.ES_TAX_INDEX, body=dsl)
+    sibling_groups = [{
+        "concept_id": g['_source'].get("concept_id"),
+        "taxonomy_id": g['_source'].get("legacy_ams_taxonomy_id"),
+        "label": g['_source'].get("label"),
+        "description": g['_source'].get("description")
+    } for g in es_result.get('hits', {}).get('hits', [])]
+    return {
+        "occupation_field": parent_id, "occupation_field_label": parent_label,
+        "groups": sibling_groups
+    }
+
+
 def get_occupations_from_text(headline, text):
     # Get occupations from textdocenrich
     header = {
@@ -47,7 +85,7 @@ def _get_groups(occupation, number_of_gropus):
         "size": 0,
         "query": {
             "term": {
-                "keywords.enriched.occupation.raw": occupation
+                "keywords.enriched.occupation.raw": occupation.lower()
             }
         },
         "aggs": {
@@ -63,11 +101,12 @@ def _get_groups(occupation, number_of_gropus):
     aggs_results = es_result.get('aggregations', {}).get('groups', {}).get('buckets', [])
     for r in aggs_results:
         g = _load_group_data(r.get('key'))
-        r['concept_id'] = g['concept_id']
-        r['taxonomy_id'] = g['legacy_ams_taxonomy_id']
-        r['label'] = g['label']
-        r['description'] = g['description']
-        r['parent'] = g['parent']
+        r['concept_id'] = g.get('concept_id')
+        r['taxonomy_id'] = g.get('legacy_ams_taxonomy_id')
+        r['label'] = g.get('label')
+        r['description'] = g.get('description')
+        r['parent_concept_id'] = g.get('parent', {}).get('concept_id')
+        r['parent_label'] = g.get('parent', {}).get('label')
         del r['key']
 
     return aggs_results
@@ -79,8 +118,8 @@ def _load_group_data(concept_id):
 
 
 if __name__ == '__main__':
-    enrichments = get_occupations_from_text("Systemutvecklare", "Vi letar efter en vass javautvecklare. Du ska vara noggrann, snabb och trevlig. Du ska kunna Java, Python, AngularJS och Cobol. Du kommer att jobba på vårt kontor i Ystad. Huvudkontoret ligger i Kiruna")
-    occupations = [o['term'] for o in enrichments.get('enriched_candidates', {}).get('occupations', [])]
-    results = load_groups_for_occupations(occupations, 5)
+    enrichments = get_occupations_from_text("Systemutvecklare",
+                                            "Vi letar efter en vass javautvecklare. Du ska vara noggrann, snabb och trevlig. Du ska kunna Java, Python, AngularJS och Cobol. Du kommer att jobba på vårt kontor i Ystad. Huvudkontoret ligger i Kiruna")
+    occs = [o['term'] for o in enrichments.get('enriched_candidates', {}).get('occupations', [])]
+    results = load_groups_for_occupations(occs, 5)
     print(json.dumps(results, indent=2))
-
--- e2tg/webapp.py
+++ e2tg/webapp.py
@@ -1,16 +1,40 @@
 from e2tg import app, main
-from flask import render_template, request
+from flask import render_template, request, jsonify
 
 
 @app.route('/', methods=['GET', 'POST'])
-def app():
-    headline = request.form.get('doc_headline', '')
-    text = request.form.get('doc_text', '')
+def application():
+    headline = request.values.get('headline', '')
+    text = request.values.get('text', '')
+    groups = _find_groups(headline, text)
+    return render_template('index.html', headline=headline, text=text, groups=groups)
+
+
+@app.route('/suggest', methods=['GET', 'POST'])
+def find_groups():
+    headline = request.values.get('headline', '')
+    text = request.values.get('text', '')
+    num = request.values.get('num', 5)
+    return jsonify(_find_groups(headline, text, num))
+
+
+@app.route('/siblings/<group_id>', methods=['GET'])
+def get_parent_groups(group_id):
+    sibling_groups = main.load_sibling_groups(group_id)
+    return jsonify(sibling_groups)
+
+
+@app.route('/show-siblings/<group_id>', methods=['GET'])
+def show_siblings(group_id):
+    sibling_groups = main.load_sibling_groups(group_id)
+    return render_template('siblings.html', siblings=sibling_groups)
+
+
+def _find_groups(headline, text, num=5):
     groups = {}
     if text:
         enrichments = main.get_occupations_from_text(headline, text)
-        occupations = [o['term']
+        occupations = [o['concept_label']
                        for o in enrichments.get('enriched_candidates', {}).get('occupations', [])]
-        groups = main.load_groups_for_occupations(occupations, 5)
-
-    return render_template('index.html', headline=headline, text=text, groups=groups)
+        groups = main.load_groups_for_occupations(occupations, num)
+    return groups
